7b6e4ac61f177bc1228fe5fd9e833e9f2d4f6843
Emelie Karlsson <emelie.sofie@gmail.com>
2017-09-12 10:58:58 +0200

    # WARNING: head commit changed in the meantime
    hello

--- se/emelie/test/Test.java
+++ se/emelie/test/Test.java
@@ -2,8 +2,8 @@ package se.emelie.test;
 
 public class Test {
     public static void main(String[] args) {
-		System.out.println("Hello there!");
-		// making some changes
-    // more changes
-	}
+	System.out.println("Hello there!");
+	// making some changes
+	// more changes
+    }
 }
